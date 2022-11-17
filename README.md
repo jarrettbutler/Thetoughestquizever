cea9b60025e3ffd0c1a482961e7649f7f6e34b25
jarrettbutler <jbutler98@hotmail.com>
2022-11-17 17:54:08 -0400

    I got as much of the JavaScript I could done before running out of time for submission.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Thetoughestquizever
-This will be the toughest quiz you will ever encounter and it is all for my bootcamp
+
+## Description
+This was a tough challenge for me mentally. I tried doing the exercise a couple different ways but couldn't get it to work I was trying to find help online but everytime I would try doing it the way they would say I would have to restart almost the entire code cause they were doing it a different way this method worked the best for me even though I ran out of time. I got so far as to start my quiz and you can read the questions but the answers say undefined and couldn'y figure out how to change that. One positive though is that I came out of this with a better understanding of CSS things started to click for me.
+
+## Installation
+
+you can download my code at my github repo found below:
+
+https://github.com/jarrettbutler/Thetoughestquizever
+
+Screenshots of my website when functioningish
+<img src="./assets/images/Screenshot.jpg" alt="My awesome start screen">
+<img src="./assets/images/Screenshot2.jpg" alt="My awesome start screen">
+
+## Usage
+If it worked you could do a quiz on basic HTML/CSS/JavaScript.
+
+## License
+Please check github repo for license
--- assets/script.js
+++ assets/script.js
@@ -1,77 +1,171 @@
-//grabbing and hooking into all the points in our html as well as defining all our variables
-var quizEl = document.getElementById("quiz");
-var rulesEl = document.getElementById("rules");
-var endGameEl = document.getElementById("endGame")
-var scoreEl = document.getElementById("score")
-var formInitials = document.getElementById("initials-form")
-var highscoresEl = document.getElementById("highscores")
-var viewHighscoreEl = document.getElementById("viewHighscore")
-var listHighscoreEl = document.getElementById("highscoreList")
-var rightEl = document.getElementById("right")
-var wrongEl = document.getElementById("wrong")
-
-var btnstartEl = document.querySelector("#startBtn");
-var btnRestartEl = document.querySelector("#restart")
-
-var questionsEl = document.getElementById("#questions")
-var answerEl = document.getElementById("#answer")
-var timerEl = document.querySelector("#timer");
-
-var score = 0;
+var startEl = document.getElementById('start');
+var startBtn = document.getElementById('startBtn');
+var questionEl = document.getElementById('question');
+var answerbuttonsEl = document.getElementById('answer-buttons');
+var wrongEl = document.getElementById('wrong');
+var correctEl = document.getElementById('correct');
+var scoreEl = document.getElementById('score')
+var viewHighscoreEl = document.getElementById('viewHighscore');
+var timerEl = document.getElementById('timer');
 var timeleft;
-var gameover;
-var highscores = [];
-var arrayShuffledQuestions;
 var questionIndex = 0;
-timerEl.innerText = 0;
+var score = 0
 
-//The array of array of questions
-var questions = [
+//My array of array of questions
+var questionArr = [
   {
     q: 'What does HTML stand for?',
     a: 'c) HyperText Markup Language',
-    choices: [{ choice: 'a) Home Tool Markup Language' }, { choice: 'b) Hyperlinks and Text Markup Language' }, { choice: 'c) HyperText Markup Language' }, { choice: 'd) I have no sweet clue' }]
+    choices: [{ choice1: 'a) Home Tool Markup Language' }, { choice2: 'b) Hyperlinks and Text Markup Language' }, { choice3: 'c) HyperText Markup Language' }, { choice4: 'd) I have no sweet clue' }]
   },
-
   {
     q: 'What does CSS stand for?',
     a: 'a) Cascading Style Sheet',
-    choices: [{ choice: 'a) Cascading Style Sheet' }, { choice: 'b) Colorful Style Sheet' }, { choice: 'c) Creative Style Sheet' }, { choice: 'd) Please for the love of god give me the answer' }]
+    choices: [{ choice1: 'a) Cascading Style Sheet' }, { choice2: 'b) Colorful Style Sheet' }, { choice3: 'c) Creative Style Sheet' }, { choice4: 'd) Please for the love of god give me the answer' }]
   },
-
   {
     q: 'Where is the most likely spot to find JavaScript inserted?',
     a: 'b) The <body> section',
-    choices: [{ choice: 'a) The <head> section' }, { choice: 'b) The <body> section' }, { choice: 'c) Both' }, { choice: 'd) Why must you make these quizzes so hard' }]
+    choices: [{ choice1: 'a) The <head> section' }, { choice2: 'b) The <body> section' }, { choice3: 'c) Both' }, { choice4: 'd) Why must you make these quizzes so hard' }]
   },
-
   {
     q: 'How do you make an alert box in JavaScript?',
     a: 'a) alert()',
-    choices: [{ choice: 'a) alert()' }, { choice: 'b) alrt()' }, { choice: 'c) alertBox()' }, { choice: 'd) Wow I am about to give up' }]
+    choices: [{ choice1: 'a) alert()' }, { choice2: 'b) alrt()' }, { choice3: 'c) alertBox()' }, { choice4: 'd) Wow I am about to give up' }]
   },
-
   {
     q: 'Which is a way to define a variable?',
     a: 'c) var',
-    choices: [{ choice: 'a) constant' }, { choice: 'b) variable' }, { choice: 'c) var' }, { choice: 'd) Got me again' }]
+    choices: [{ choice1: 'a) constant' }, { choice2: 'b) variable' }, { choice3: 'c) var' }, { choice4: 'd) Got me again' }]
   },
-
   {
     q: 'Does CSS suck?',
     a: 'b) Yes',
-    choices: [{ choice: 'a) No' }, { choice: 'b) Yes' }, { choice: 'c) No' }, { choice: 'd) You know the right answer' }]
+    choices: [{ choice1: 'a) No' }, { choice2: 'b) Yes' }, { choice3: 'c) No' }, { choice4: 'd) You know the right answer' }]
   },
-
   {
     q: 'Which HTML element is used to specify the footer of a document?',
     a: 'c) footer',
-    choices: [{ choice: 'a) section' }, { choice: 'b) bottom' }, { choice: 'c) footer' }, { choice: 'd) I give up' }]
+    choices: [{ choice1: 'a) section' }, { choice2: 'b) bottom' }, { choice3: 'c) footer' }, { choice4: 'd) I give up' }]
   },
-
   {
     q: 'Are you happy this is the last question?',
     a: 'a) Yes',
-    choices: [{ choice: 'a) Yes' }, { choice: 'b) No' }, { choice: 'c) Maybe So' }, { choice: 'd) Finally I know this one' }]
+    choices: [{ choice1: 'a) Yes' }, { choice2: 'b) No' }, { choice3: 'c) Maybe So' }, { choice4: 'd) Finally I know this one' }]
   },
 ];
+
+//this is where it starts after you click the start button
+startBtn.addEventListener('click', function(){
+  startGame()
+  setTime()
+});
+
+//When you click the start button the timer should begin the countdown from 120
+var setTime = function () {
+  timeleft = 120;
+var timercheck = setInterval(function() {
+  timerEl.innerText = timeleft;
+  timeleft--
+  if (timeleft == 0) {
+      clearInterval(timercheck)
+  }
+  if (timeleft < 0) {
+      timerEl.innerText = 0
+      clearInterval(timercheck)
+  }
+  }, 1000)
+}
+
+//This is supossed to be where I program the questions appearing
+var startGame = function() {
+  startEl.classList.add('hide');
+  startEl.classList.remove('show');
+  questionEl.classList.remove('hide');
+  questionEl.classList.add('show');
+
+  //Shuffle questions
+  arrayShuffledQuestions = questionArr.sort(() => Math.random() - 0.5)
+  setTime()
+  setQuestion()
+}
+
+//this will get you the next question
+var setQuestion = function() {
+  resetAnswers()
+  displayQuestion(arrayShuffledQuestions[questionIndex])
+}
+
+//resete answer buttons
+var resetAnswers = function() {
+  while (answerbuttonsEl.firstChild) {
+      answerbuttonsEl.removeChild(answerbuttonsEl.firstChild)
+  };
+};
+
+//displays the question
+var displayQuestion = function(index) {
+  questionEl.innerText = index.q
+  for (var i = 0; i < index.choices.length; i++) {
+      var answerbutton = document.createElement('button')
+      answerbutton.innerText = index.choices[i].choice
+      answerbutton.classList.add('bttn')
+      answerbutton.classList.add('answerbttn')
+      answerbutton.addEventListener("click", answerCheck)
+      answerbuttonsEl.appendChild(answerbutton)
+      }
+  };
+
+//display correct! message on screen below answers
+var answerCorrect = function() {
+  if (correctEl.className = "hide") {
+      correctEl.classList.remove("hide")
+      correctEl.classList.add("banner")
+      wrongEl.classList.remove("banner")
+      wrongEl.classList.add("hide")
+      }
+  }  
+//display wrong! message on screen below answers
+var answerWrong = function() {
+  if (wrongEl.className = "hide") {
+      wrongEl.classList.remove("hide")
+      wrongEl.classList.add("banner")
+      correctEl.classList.remove("banner")
+      correctEl.classList.add("hide")
+  }
+}
+
+//Checks to see if the answer is correct
+var answerCheck = function(event) {
+  var selectedanswer = event.target
+      if (arrayShuffledQuestions[questionIndex].a === selectedanswer.innerText){
+          answerCorrect()
+          score = score + 1
+      }
+
+      else {
+        answerWrong()
+        timeleft = timeleft - 10;
+    };
+
+ //go to next question, check if there is more questions
+ questionIndex++
+ if  (arrayShuffledQuestions.length > questionIndex + 1) {
+     setQuestion()
+ }   
+ else {
+    gameover = "true";
+    showScore();
+     }
+}
+
+  //Display total score screen at end of game
+  var showScore = function () {
+    questionEl.classList.add("hide");
+    scoreEl.classList.remove("hide");
+    scoreEl.classList.add("show");
+  
+    var scoreDisplay = document.createElement("p");
+    scoreDisplay.innerText = ("Your final score is " + score + "!");
+    containerScoreEl.appendChild(scoreDisplay);
+  } 
